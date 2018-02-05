0724a1feb487bc7718f959fb7767b4ce5f9fab80
Yeswanth <yeswantht@helixtechsolutions.com>
2018-02-05 17:58:39 +0530

    First commit

---
+++ SQLExpenseMngmt.sql
@@ -0,0 +1,5 @@
+select * from expense_details
+select * from Employee
+delete from Travel_Details where MR_No=1011
+alter table expense_details alter column Amount_spent int 
+insert into expense_details  values(1,'Travel Fare','2017/12/13',400,'Cash',1001,1001,'Pending',101)
